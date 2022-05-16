4bd26357e1a43d58f618465bd761c399aba337cb
Cast4nha <felipevcast@unifesspa.edu.br>
2022-05-16 14:47:56 -0300

    First commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+
+        /*     int a = 8;
+        int b = 12;
+        System.out.println("Hello World" + (a-b));
+*/
+    }
+}
+
